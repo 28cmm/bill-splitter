e174f773d525955bc51305203c94c8e343a5907f
28cmm <joshuafang1998@gmail.com>
2019-01-20 21:39:18 -0800

    Initial Commit

---
+++ Bill Splitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bill Splitter
+//
+//  Created by Yilei Huang on 2019-01-20.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
